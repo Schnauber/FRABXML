464fbbd05575d2df8cf2b8b4490442e9b93a4be0
elena.wolf <elena.wolf@kit.edu>
2020-04-22 15:38:53 +0200

    adapted path

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import xmlschema
 from pprint import pprint
 
-FRAB_Schema = xmlschema.XMLSchema('xsd/schedule.xsd')
+FRAB_Schema = xmlschema.XMLSchema('xml/schedule.xsd')
 
 pprint(FRAB_Schema.to_dict('xml/schedule.xml'))
 
